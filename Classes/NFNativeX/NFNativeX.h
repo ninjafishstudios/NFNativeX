66e61ab368fb16a83eaa0954169358a18398a736
williamlocke <williamlocke@me.com>
2014-03-21 02:18:58 -0400

    --

--- Classes/NFNativeX/NFNativeX.h
+++ Classes/NFNativeX/NFNativeX.h
@@ -9,9 +9,14 @@
 
 @interface NFNativeX : NSObject
 
+typedef void (^ NFNativeXShowImpressionCompletionHandler)(BOOL installed);
+
+@property (nonatomic, copy) NSString *appId;
+
+
 + (NFNativeX *)sharedInstance;
-
-
 - (void)showInterstitial:(NSString *)label;
+-(void)showInterstitial:(NSString *)label completionHandler:(NFNativeXShowImpressionCompletionHandler)completionHandler;
+
 
 @end
--- Classes/NFNativeX/NFNativeX.m
+++ Classes/NFNativeX/NFNativeX.m
@@ -6,9 +6,12 @@
 //
 
 #import "NFNativeX.h"
+#import "NativeXSDK.h"
 
-@interface NFNativeX(){
 
+
+@interface NFNativeX() <NativeXSDKDelegate, NativeXAdViewDelegate>{
+    NFNativeXShowImpressionCompletionHandler _showImpressionCompletionHandler;
 }
 @end
 
@@ -41,6 +44,89 @@
 }
 
 
+
+
+- (void)showInterstitial:(NSString *)label{
+
+    [self showInterstitial:label completionHandler:nil];
+    
+}
+
+-(void)showInterstitial:(NSString *)label completionHandler:(NFNativeXShowImpressionCompletionHandler)completionHandler{
+    _showImpressionCompletionHandler = completionHandler;
+    
+    
+    [[NativeXSDK sharedInstance] setDelegate:self];
+    [[NativeXSDK sharedInstance] createSessionWithAppId:self.appId];
+    //    [[NativeXSDK sharedInstance] fetchAdWithPlacement:kAdPlacementGameLaunch delegate:self];
+    //    // Fetch the interstitial ad.
+    [[NativeXSDK sharedInstance] showAdWithCustomPlacement:label];
+}
+
+
+
+
+#pragma mark <NativeXSDKDelegate>
+// Called if the SDK initiates successfully
+- (void)nativeXSDKDidCreateSession{
+
+}
+
+// Called if the SDK fails to initiate.
+- (void)nativeXSDKDidFailToCreateSession:(NSError *)error{
+
+}
+
+- (void)nativeXAdView:(NativeXAdView *)adView didLoadWithPlacement:(NSString *)placement
+{
+    //Called when an ad has been loaded/cached and is ready to be shown
+    
+    // If you call displayAdView method here the SDK will show the ad as soon as it's ready
+    [adView displayAdView];
+}
+
+
+/** called if no ad is available at this time
+ *
+ * @param adView        the NativeX adView that has NOT been loaded
+ */
+- (void)nativeXAdViewNoAdContent:(NativeXAdView *)adView{
+    
+    NSLog(@"native x ad view no ad content");
+    
+    if(_showImpressionCompletionHandler){
+        _showImpressionCompletionHandler(NO);
+        _showImpressionCompletionHandler = nil;
+    }
+}
+
+/** Called when error loading an ad (was the SDK initialized correctly?)
+ *
+ * @param adView        the NativeX adView that has NOT been loaded because of an error
+ * @param error         reason why ad failed to load
+ */
+- (void)nativeXAdView:(NativeXAdView *)adView didFailWithError:(NSError *)error{
+    NSLog(@"native x ad view did fail with error");
+    
+    if(_showImpressionCompletionHandler){
+        _showImpressionCompletionHandler(NO);
+        _showImpressionCompletionHandler = nil;
+    }
+    
+}
+
+
+/** called after an ad has been displayed on screen
+ *
+ * @param adView        the NativeX adView that displayed
+ */
+- (void)nativeXAdViewDidDisplay:(NativeXAdView *)adView{
+    NSLog(@"ad view did display");
+    if(_showImpressionCompletionHandler){
+        _showImpressionCompletionHandler(YES);
+        _showImpressionCompletionHandler = nil;
+    }
+}
 
 
 
